dfb0834870180c5d358de54d84b6cfefd1358f44
Abdulaziz R <razabdulaziz77@gmail.com>
2026-03-26 15:07:30 +0500

    v1.3

--- main.cpp
+++ main.cpp
@@ -1,20 +1,3 @@
-/*
-Здесь я продумаю архитектуру моего проекта. 
-1. Хорошо сначала нужно определиться с первыми состояниями которые будет иметь задача. 
-2. Потом с архитектурой, а именно сколько файлов в первом этапе, потом что и тд.
-3. И исполнение
-
-Я тут подумал что мой этот проект будет иметь больше версий, потому что я тут подумал что я хочу попробовать его развивать 
-так что мне потребуется сразу определиться с версиями.
-В первой версии я хочу сделать базу, типу состояния первые задать, также первые 4 команды, и как будет само исполнения
-и все пока что писать в одном файле main.cpp
-Во второй версии я уже подумывал расширить и состояния и команды, и попробовать использовать уже .h (header file)
-В третьей версии я уже буду работать над более детальной структурой и версии задач, по типу description и другие более
-complicated вещи, и буду упрощать структуру, добавлю доп файлы, также подключу фичи.
-Но начиная уже с 3 и более версией я врядли буду заниматься до лета, потому что мне нужно научиться а усложнять я буду
-только когда наберусь опыта и улучшу уже там все проекты.
-*/
-
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -31,17 +14,88 @@ struct Task {
 
 vector<Task> Tasks;
 
-void addTask(){}
-void deleteTask(){}
-void updateTask(){}
-void listTasks(){}
-void saveTasks(){}
-void uploadTasks(){}
-int getId(){}
+void addTask(string& title);
+void deleteTask(string& title);
+void updateTaskT(string& o_title, string& n_title);
+void updateTaskD(string& title, string& description);
+void updateTaskS(string& title, string& status);
+void listTasks();
+
+void saveTasks();
+void uploadTasks();
+int getId(){
+    int id = 0;
+    if (!Tasks.empty()){
+        id = Tasks[Tasks.size() - 1].id + 1;
+    }
+    return id;
+}
 
 int main(int argc, const char* argv[]){
 
-    //
+    // ./{name_of_file} {command} {details}
+    // [      0       ] [   1   ] [   2   ]
+
+    if (argc < 2){
+        cout << "Incorrect input\n";
+        return 1;
+    }
+
+    uploadTasks();
+
+    string command = argv[1];
+    if (command == "add")
+    {
+        string title = argv[2];
+        addTask(title);
+        saveTasks();
+    }
+    else if (command == "update")
+    {
+        string type = argv[2];
+        if (type == "title")
+        {
+            string title = argv[3];
+            string n_title = argv[4];
+            updateTaskT(title, n_title);
+            saveTasks();
+        }
+        else if (type == "description")
+        {
+            string title = argv[3];
+            string description = argv[4];
+            updateTaskD(title, description);
+            saveTasks();
+        }
+        else if (type == "status")
+        {
+            string title = argv[3];
+            string status = argv[4];
+            updateTaskS(title, status);
+            saveTasks();
+        }
+        else
+        {
+            cout << "Incorrect input\n";
+            return 1;
+        }
+    }
+    else if (command == "delete")
+    {
+        string title = argv[2];
+        deleteTask(title);
+        saveTasks();
+    }
+    else if (command == "list")
+    {
+        listTasks();
+        return 0;
+    }
+    else
+    {
+        cout << "Incorrect input\n";
+        return 1;
+    }
 
     return 0;
 }
@@ -54,7 +108,7 @@ void addTask(string& title){
     newTask.status = "todo";
     Tasks.push_back(newTask);
 }
-void deleteTask(string& title){
+void deleteTask(string& title){ // needs rebuilding
     for (int i = 0; i < Tasks.size(); i++){
         if (title == Tasks[i].title){
             Tasks.erase(Tasks.begin() + i);
@@ -116,17 +170,11 @@ void uploadTasks(){
     }
     file.close();
 }
+
 void saveTasks(){
     ofstream file("data.txt", ios::app);
     for (int i = 0; i < Tasks.size(); i++){
         file << Tasks[i].id << "|" << Tasks[i].title << "|" << Tasks[i].description << "|" << Tasks[i].status << "\n";
     }
     file.close();
-}
-int getId(){
-    int id = 0;
-    if (!Tasks.empty()){
-        id = Tasks[Tasks.size() - 1].id + 1;
-    }
-    return id;
 }
